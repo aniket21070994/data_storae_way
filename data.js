2d366498cb13005e7b4662db66022089b49b8342
aniket21070994 <aniket.mankar001@gmail.com>
2024-04-07 00:39:11 +0530

    new way to store data

---
+++ data.js
@@ -0,0 +1,112 @@
+const data=[
+
+    {
+        name:"filters-project",
+        img:"filters-project.png",
+        desc:"",
+        id:"",
+        link:"https://avinash10098.github.io/filters-project/"
+    },
+
+    {
+        name:"grocery-bud",
+        img:"grocery-bud.png",
+        desc:"",
+        id:"",
+        link:"https://avinash10098.github.io/grocery-bud/"
+    },
+
+    {
+        name:"Menu-project",
+        img:"Menu-project.png",
+        desc:"",
+        id:"",
+        link:"https://avinash10098.github.io/Menu-project/"
+    },
+
+    {
+        name:"Modal",
+        img:"Modal.png",
+        desc:"",
+        id:"",
+        link:"https://avinash10098.github.io/Modal/"
+    },
+
+    {
+        name:"Navbar",
+        img:"Navbar.png",
+        desc:"",
+        id:"",
+        link:"https://avinash10098.github.io/Navbar/"
+    },
+
+    {
+        name:"Q-A",
+        img:"Q-A.png",
+        desc:"",
+        id:"",
+        link:"https://avinash10098.github.io/Q-A/"
+   },
+
+    {
+        name:"random-jokes-4",
+        img:"random-jokes-4.png",
+        desc:"",
+        id:"",
+        link:"https://avinash10098.github.io/random-jokes-4/"
+    },
+
+    {
+        name:"Reviews",
+        img:"Reviews.png",
+        desc:"",
+        id:"",
+        link:"https://avinash10098.github.io/Reviews/"
+    },
+
+    {
+        name:"scroll-project",
+        img:"scroll-project.png",
+        desc:"",
+        id:"",
+        link:"https://avinash10098.github.io/scroll-project/"
+    },
+
+    {
+        name:"slider-es6",
+        img:"slider-es6.png",
+        desc:"",
+        id:"",
+        link:"https://avinash10098.github.io/slider-es6/"
+    },
+
+    {
+        name:"slider",
+        img:"slider.png",
+        desc:"",
+        id:"",
+        link:"https://avinash10098.github.io/slider/"
+    },
+
+    {
+        name:"tabs",
+        img:"tabs.png",
+        desc:"",
+        id:"",
+        link:"https://avinash10098.github.io/Tabes/"
+    },
+
+    {
+        name:"times",
+        img:"times.png",
+        desc:"",
+        id:"",
+        link:"https://avinash10098.github.io/timer/"
+    }
+
+]
+
+
+
+
+
